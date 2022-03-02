420d90bd2501615b7c745d5d6cf60c279660bc1d
davidliyutong <davidliyutong@sjtu.edu.cn>
2022-03-02 13:14:02 +0000

    Add commandline args to run.sh

--- Projects/Project3/run.sh
+++ Projects/Project3/run.sh
@@ -1,7 +1,17 @@
 if [ ! -d "./build" ]; then
-mkdir build
+    mkdir build
 fi
 cd build && cmake .. && make && cd ..
 
-python3 ./tests/test_generate_data.py 1000
-mpirun -n 5 ./build/test_MatMulMPI M.csv N.csv result.csv
+if [ $# -ge 2 ]; then
+    MAT_SIZE=$1
+    N_PROCS=$2
+else
+    MAT_SIZE=1000
+    N_PROCS=5
+fi
+echo "Matrix size = $MAT_SIZE x $MAT_SIZE "
+echo "N_PROCS = $N_PROCS"
+
+python3 ./tests/test_generate_data.py $MAT_SIZE
+mpirun -n $N_PROCS ./build/test_MatMulMPI M.csv N.csv result.csv
